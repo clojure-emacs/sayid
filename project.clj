2fd756d9cb705eb4ae1211afed3cc7d4fa7576f5
Bill Piel <bill@billpiel.com>
2015-11-05 16:15:48 -0500

    add deps: clj 1.7, puget, swiss-arrows

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [mvxcvi/puget "0.8.1"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [mvxcvi/puget "0.8.1"]
+                 [swiss-arrows "1.0.0"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]]
                    :plugins [[lein-midje "3.1.3"]]}})
