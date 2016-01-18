9d645c4732f09bbac466df340524311ad118ac43
Bill Piel <bill@billpiel.com>
2016-01-18 09:03:13 -0500

    add testing profiles for other versions of clojure

--- project.clj
+++ project.clj
@@ -7,4 +7,9 @@
                  [mvxcvi/puget "0.8.1"]
                  [swiss-arrows "1.0.0"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]
-                   :plugins [[lein-midje "3.1.3"]]}})
+                   :plugins [[lein-midje "3.1.3"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
+  :aliases {"test-all" ["with-profile" "+1.4:+1.5:+1.6:+1.7" "midje"]})
