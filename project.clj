6d70aa8d035e6151415921361a675bebf0576001
Bill Piel <bill@billpiel.com>
2016-03-07 21:18:27 -0500

    add 1.8 to  lein alias

--- project.clj
+++ project.clj
@@ -12,5 +12,6 @@
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
-  :aliases {"test-all" ["with-profile" "+1.4:+1.5:+1.6:+1.7" "midje"]})
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+  :aliases {"test-all" ["with-profile" "+1.4:+1.5:+1.6:+1.7:+1.8" "midje"]})
