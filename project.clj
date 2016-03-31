460470f2e6426a6ead93b84ded19f35cd1f7d20e
Bill Piel <bill@billpiel.com>
2016-03-31 07:25:42 -0400

    rollback to clj 1.6 and puget 0.8.1

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [mvxcvi/puget "1.0.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [mvxcvi/puget "0.8.1"]
                  [swiss-arrows "1.0.0"]
                  [org.clojure/tools.reader "1.0.0-alpha3"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]
