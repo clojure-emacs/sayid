7d12cffc6b929e130e18073abf36480f9b5c8391
Bill Piel <bill@billpiel.com>
2016-06-14 04:58:09 -0400

    print children for nodes that are outside of 'view'

--- src/com/billpiel/sayid/string_output.clj
+++ src/com/billpiel/sayid/string_output.clj
@@ -339,37 +339,35 @@
   [tree]
   (if (nil? tree)
     []
-    (when-let [view (*view* tree)]
-      (let [trace-root? (-> tree meta :trace-root)
-            has-children (some-> tree
-                                 :children
-                                 not-empty)]
-        [(when (not trace-root?)
-           (name->string tree true)) "\n"
-           (when-let [selects (:selects view)]
-             (selects-str tree selects))
-           (when-sel :args (args*-str tree))
-           (when has-children
-             [(when-sel :return (return-str tree :pos :before))
-              (when-sel :throw (throw-str tree))
-              (mapv tree->string* (:children tree))
-              (when (not trace-root?)
-                [(name->string tree false) "\n"])
-              (when-sel :args
-                        (args*-str tree))])
-           (when-sel :return (return-str tree :pos :after))
-           (when-sel :throw (throw-str tree))
-           (when (and (-> tree :depth nil? not)
-                      (-> tree
-                          meta
-                          ::ws/workspace
-                          not))
-             [(get-line-meta tree) ;; clear meta
-              (when (not trace-root?)
-                (slinky-pipes-MZ (:depth tree)
-                                 :end "^"))])
-           reset-color-code
-           "\n"]))))
+    (let [view (*view* tree)
+          trace-root? (-> tree meta :trace-root)
+          visible? (and (not trace-root?)
+                        view)
+          has-children (some-> tree
+                               :children
+                               not-empty)]
+      [(when visible?
+         [(name->string tree true) "\n"
+          (when-let [selects (:selects view)]
+            (selects-str tree selects))
+          (when-sel :args (args*-str tree))])
+       (when has-children
+         [(when-sel :return (return-str tree :pos :before))
+          (when-sel :throw (throw-str tree))
+          (mapv tree->string* (:children tree))
+          (when (not trace-root?)
+            [(name->string tree false) "\n"])
+          (when-sel :args
+                    (args*-str tree))])
+       (when visible?
+         [(when-sel :return (return-str tree :pos :after))]
+         (when-sel :throw (throw-str tree))
+         [(get-line-meta tree) ;; clear meta
+          (when (not trace-root?)
+            (slinky-pipes-MZ (:depth tree)
+                             :end "^"))])
+       reset-color-code
+       "\n"])))
 
 (defn tree->string
   [tree]
