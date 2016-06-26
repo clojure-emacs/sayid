11e9486e81eeee88bc9cd71e67a736c894da69cf
Bill Piel <bill@billpiel.com>
2016-06-25 20:34:46 -0400

    lein plugin

---
+++ src/sayid/plugin.clj
@@ -0,0 +1,11 @@
+(ns sayid.plugin)
+
+(defn middleware
+  [project]
+  (-> project
+      (update-in [:dependencies]
+                 (fnil into [])
+                 [['com.billpiel/sayid "0.0.3-dev"]])
+      (update-in [:repl-options :nrepl-middleware]
+                 (fnil into [])
+                 ['com.billpiel.sayid.nrepl-middleware/wrap-sayid])))
