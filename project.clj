fed94016d2df3e86ca6644aac01eafe68706485f
Bill Piel <bill@billpiel.com>
2016-06-27 16:08:22 -0400

    bump to v0.0.5-dev

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.billpiel/sayid "0.0.4"
+(defproject com.billpiel/sayid "0.0.5-dev"
   :description "Sayid is a library for debugging and profiling clojure code."
   :signing {:gpg-key "<bill@billpiel.com>"}
   :url "http://bpiel.github.io/sayid/"
