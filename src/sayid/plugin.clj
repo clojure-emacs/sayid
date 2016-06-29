8cb46ae679561122b676e29028551025e55bbab6
Bill Piel <bill@billpiel.com>
2016-06-29 05:44:30 -0400

    bump to v0.0.7-dev

--- src/sayid/plugin.clj
+++ src/sayid/plugin.clj
@@ -5,7 +5,7 @@
   (-> project
       (update-in [:dependencies]
                  (fnil into [])
-                 [['com.billpiel/sayid "0.0.6"]])
+                 [['com.billpiel/sayid "0.0.7-dev"]])
       (update-in [:repl-options :nrepl-middleware]
                  (fnil into [])
                  ['com.billpiel.sayid.nrepl-middleware/wrap-sayid])))
