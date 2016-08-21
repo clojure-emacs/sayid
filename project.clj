37706c148a93fd04af5b15adf3a1b465db231f76
Bill Piel <bill@billpiel.com>
2016-08-20 20:56:16 -0400

    bump tamarin version. Drop cide-repl plugin from dev profile

--- project.clj
+++ project.clj
@@ -5,15 +5,14 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [tamarin "0.1.0"]
+                 [tamarin "0.1.1-dev"]
                  [org.clojure/tools.reader "1.0.0-alpha3"]
                  [org.clojure/tools.namespace "0.2.11"]]
   :repl-options {:nrepl-middleware [com.billpiel.sayid.nrepl-middleware/wrap-sayid]}
   :profiles {:dev {:dependencies [[midje "1.8.3"]
                                   [org.clojure/tools.nrepl "0.2.10"]]
                    :plugins [[lein-midje "3.1.3"]
-                             [lein-codox "0.9.4"]
-                             [cider/cider-nrepl "0.10.0"]]}
+                             [lein-codox "0.9.4"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
