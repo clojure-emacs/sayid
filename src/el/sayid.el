5ee0b79b5f214f21596b59318c2ed561c334e9f5
Bill Piel <bill@billpiel.com>
2017-02-08 08:48:43 -0500

    bump el version

--- src/el/sayid.el
+++ src/el/sayid.el
@@ -3,7 +3,7 @@
 ;; Copyright (c) 2016 Bill Piel
 
 ;; Author: Bill Piel <bill@billpiel.com>
-;; Version: 0.0.11
+;; Version: 0.0.12
 ;; URL: https://github.com/bpiel/sayid
 
 ;; Licensed under the Apache License, Version 2.0 (the "License");
