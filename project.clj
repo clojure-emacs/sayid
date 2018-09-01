56ec0343322cf30d689810a8f5cffee004bb8d07
Bozhidar Batsov <bozhidar@batsov.com>
2018-09-01 09:44:45 +0300

    Update the deployment config

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
 (defproject com.billpiel/sayid "0.0.17"
   :description "Sayid is a library for debugging and profiling clojure code."
-  :signing {:gpg-key "<bill@billpiel.com>"}
   :url "http://bpiel.github.io/sayid/"
   :scm {:name "git" :url "https://github.com/clojure-emacs/sayid"}
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :aot [com.billpiel.sayid.sayid-multifn]
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password
+                                    :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [tamarin "0.1.2"]
                  [org.clojure/tools.reader "1.3.0-alpha3"]
