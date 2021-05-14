4261e5b75949865ed7a1ebd9cdab335556139283
Mark Dawson <markgdawson@gmail.com>
2021-05-14 09:36:31 +0100

    magit integration

---
+++ src/el/sayid-magit.el
@@ -0,0 +1,46 @@
+;;; sayid-magit.el --- Choose sayid tracing from magit -*- lexical-binding: t -*-
+
+;; Author: Mark Dawson
+;; Maintainer: Bill Piel <bill@billpiel.com>
+;; Version: 0.0.1
+;; URL: https://github.com/clojure-emacs/sayid
+;; Package-Requires: ((cider "0.21.0"))
+
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+
+;;     http://www.apache.org/licenses/LICENSE-2.0
+
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
+;;; Commentary:
+
+;;; Code:
+
+;;;###autoload
+(defun sayid-magit--trace-ns-in-files (file-names)
+  "Trace namespace in FILE-NAMES."
+  (mapcar (lambda (file-name)
+            (with-current-buffer (find-file-noselect
+                                  (expand-file-name file-name))
+              (nrepl-send-sync-request (list "op" "sayid-trace-ns-in-file"
+                                             "file" (buffer-file-name))
+                                       (cider-current-connection))))
+          file-names)
+  (sayid-show-traced))
+
+;;;###autoload
+(defun sayid-magit-trace-changed-ns ()
+  "Trace the changed namespaces in a git commit."
+  (interactive)
+  (sayid--trace-ns-in-files
+   (magit-changed-files (magit-read-starting-point "Sayid trace" nil "HEAD"))))
+
+(provide 'sayid-magit)
+
+;;; sayid-magit.el ends here
